6fdec06c26738b3e09ea345779fbc32bbcc7b6b1
kyllene@kaist.ac.kr <77186601+dooho0915@users.noreply.github.com>
2022-07-10 23:15:05 +0900

    Update App.js
    0710 dooho make home screen

--- ProjectName/App.js
+++ ProjectName/App.js
@@ -1,112 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  View, TouchableOpacity, FlatList
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const styles = StyleSheet.create({
+  ButtonContainer: {
+    height: 155+12+16+16,
+    width: 155,
+    //backgroundColor: "lightblue",
+    display: 'flex',
+    alignItems: 'flex-start',
+  },
+  PictureContainer: {
+    backgroundColor: "#F5F5F5",
+    borderRadius: 16,
+    width: 155,
+    height: 155,
+  },
+  Name: {
+    color: "#212121",
+    //backgroundColor: "lightblue",
+    fontWeight: 'bold',
+    fontSize: 16,
+    left: 8,
+    top: 12,
+    width: 139,
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+  },
+  Info: {
+    color: '#8F8F8F',
+    //backgroundColor: "pink",
+    fontSize: 12,
+    top: 16,
+    left: 8,
+    width: 139,
+  },
+  DiscountBar: {
+    backgroundColor: "#4769EE",
+    position: 'absolute',
+    height: 27,
+    top: 24, left: 75,
+    width: 80, borderBottomLeftRadius: 10, borderTopLeftRadius: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  DiscountNumber:{
+    color: "white",
+    fontSize: 16,
+    fontWeight: '500',
+    right: 8,
+  },
+  RowContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 32,
+    marginVertical: 10,
+    justifyContent: 'space-between',
+    //alignItems: 'center',
+  },
+  UnsignedContainer: {
+    backgroundColor: "#B1AEAE",
+    marginHorizontal: 32,
+    height: 72,
+    alignItems: 'center',
+    flexDirection: 'row', 
+    justifyContent: 'center',
+    borderRadius: 16, 
+  },
+  UnsignedText: {
+    fontWeight: '700',
+    fontSize: 16,
+    color: "white",
+  },
+});
+
+const UnsignedHeader = ({onPress}) => (
+  <TouchableOpacity onPress ={onPress} style={styles.UnsignedContainer}>
+    <Text style={styles.UnsignedText}>관람 인증 후 쿠폰을 자유롭게 이동하세요!</Text>
+  </TouchableOpacity>
+);
+
+const AppButton = ({ onPress }) => (
+  <View>
+  <TouchableOpacity onPress={onPress} style={styles.ButtonContainer}>
+    <View style={styles.PictureContainer}></View>
+    <View style={styles.DiscountBar}>
+      <Text style={styles.DiscountNumber}>10~15%</Text>
     </View>
-  );
-};
+      <Text style={styles.Name}>쏘리에스프레소바</Text>
+      <Text style ={styles.Info}>카페, 디저트</Text>
+  </TouchableOpacity>
+  </View>
+);
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+const App= () => {
+  const id = [0, 1, 2]
+  const buttonList = id.map((button)=> (<View style ={styles.RowContainer}><AppButton/><AppButton/></View>))
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <SafeAreaView>
+      <UnsignedHeader></UnsignedHeader>
       <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>rn project by</Text> 
-            dooho lee
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        contentInsetAdjustmentBehavior="automatic">
+          <View>{buttonList}</View>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
