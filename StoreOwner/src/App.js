0f3f331150f19054a61d52be59acbda5d89ff6b1
kyllene@kaist.ac.kr <77186601+dooho0915@users.noreply.github.com>
2022-07-26 19:47:18 +0900

    0726 dooho
    점주 코드 인증 화면

--- StoreOwner/src/App.js
+++ StoreOwner/src/App.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useNavigation, NavigationContainer} from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from './screens/MainScreen';
 import CertScreen from './screens/CertScreen';
@@ -7,12 +8,14 @@ import CertScreen from './screens/CertScreen';
 const Stack = createStackNavigator();
 
 const App = () => {
-  const navigation=useNavigation();
+
   return (
+      <NavigationContainer>
       <Stack.Navigator screenOptions = {{ cardStyle: {backgroundColor: 'white'} }} >
         <Stack.Screen name="CERT" options={{headerShown: false}} component={CertScreen}/>
         <Stack.Screen name="MAIN" options={{headerShown: false}} component={MainScreen}/>
       </Stack.Navigator>
+      </NavigationContainer>
   );
 };
 
--- StoreOwner/src/screens/CertScreen.js
+++ StoreOwner/src/screens/CertScreen.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Text , SafeAreaView, TextInput, TouchableOpacity, View} from 'react-native';
+import React, {useState} from 'react';
+import { Text , SafeAreaView, TextInput, TouchableOpacity, View, Alert} from 'react-native';
 import { styles } from '../Style';
 
 const CertScreen = ({navigation}) => {
-    
+    const [inputcode, setInputcode] = useState('');
+    const Approval_code =['dooho', 'maru']
+        
+    const createAlert = () =>
+    Alert.alert(
+      Approval_code.includes(inputcode) ? "인증 완료되었습니다" : "잘못된 코드입니다",
+      inputcode,
+      [ Approval_code.includes(inputcode) ?
+        { text: "확인", onPress: () => navigation.navigate("MAIN") }:
+        { text: "재입력", onPress: () => console.log(inputcode) }
+      ]
+    );
     return (
         <SafeAreaView style={{flex: 1, justifyContent:"center",}}>
-            <TextInput eyboardType='numeric' placeholder="점주 코드를 입력해 주세요" placeholderTextColor="#B1AEAE" style={styles.inputbox}></TextInput>
+            <TextInput 
+                placeholder="점주 코드를 입력해 주세요" placeholderTextColor="#B1AEAE" 
+                style={styles.inputbox}
+                onChangeText={newInput => setInputcode(newInput)}
+                autoCapitalize = "none"
+                defaultValue={inputcode}/>
             <View style={{height:12,}}></View>
-            <TouchableOpacity style ={styles.button} onPress={()=>navigation.navigate('MAIN') }>
+            <TouchableOpacity style ={styles.button} onPress={createAlert}>
                 <Text style={styles.button_text}>인증하기</Text>
             </TouchableOpacity>
         </SafeAreaView>
@@ -16,6 +32,4 @@ const CertScreen = ({navigation}) => {
 };
 
 export default CertScreen;
-
-/* src/App.js */
 
