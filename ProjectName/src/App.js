65b67da28cd6d43842938dd8cd6ea456e84db79b
umsanmaru <70532311+umsanmaru@users.noreply.github.com>
2022-07-27 00:08:14 +0900

    realtime DB api for mainscreen.js completed

--- ProjectName/src/App.js
+++ ProjectName/src/App.js
@@ -1,159 +1,130 @@
-import React, {useState, useEffect, useMemo, useReducer, createContext} from 'react';
+import React, {useEffect, useMemo, useReducer, createContext} from 'react';
 import { ActivityIndicator, View, Button } from 'react-native';
-import { NavigationContainer} from '@react-navigation/native';
-import StackNavigation from './navigations/Stack';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/Feather';
+import { Platform } from 'react-native';
 
 import LoginScreen from './screens/LoginScreen';
 import MainScreen from './screens/MainScreen'
+import DetailScreen from './screens/DetailScreen';
 
 export const AuthContext = createContext();
 const Stack = createStackNavigator();
 
 async function googleSignIn() {
-    const { idToken } = await GoogleSignin.signIn();
-    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  const { idToken } = await GoogleSignin.signIn();
+  const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
-    return auth().signInWithCredential(googleCredential);
+  return auth().signInWithCredential(googleCredential);
 }
 
 async function googleSignOut() {
-    await auth().signOut()
+  await auth().signOut()
 }
 
 const App = () => {
-    const [state, dispatch] = useReducer(
-        (prevState, action) => {
-            switch (action.type) {
-                case 'RESTORE_TOKEN':
-                    return {
-                        ...prevState,
-                        userToken: prevState.userToken || action.token,
-                        isLoading: false,
-                    };
-                case 'SIGN_IN':
-                    return {
-                        ...prevState,
-                        isSignout: false,
-                        userToken: action.token,
-                    };
-                case 'SIGN_OUT':
-                    return {
-                        ...prevState,
-                        isSignout: true,
-                        userToken: null,
-                    };
-            }
-        },
-        {
-            isLoading: true,
-            isSignout: false,
-            userToken: null,
-        } 
-    );
+	const [state, dispatch] = useReducer(
+		(prevState, action) => {
+			switch (action.type) {
+				case 'RESTORE_TOKEN':
+					return {
+						...prevState,
+						userToken: action.token,
+						isLoading: false,
+					};
+				case 'SIGN_IN':
+					return {
+						...prevState,
+						isSignout: false,
+						userToken: action.token,
+					};
+				case 'SIGN_OUT':
+					return {
+						...prevState,
+						isSignout: true,
+						userToken: null,
+					};
+			}
+		},
+		{
+			isLoading: true,
+			isSignout: false,
+			userToken: null,
+		} 
+	);
 
-    const onAuthStateChanged = (user) => {
-        dispatch({ type: user ? 'SIGN_IN' : 'RESTORE_TOKEN', token: user?.uid });
-    }
+	const onAuthStateChanged = (user) => {
+		dispatch({ type: 'RESTORE_TOKEN', token: user?.uid });
+		if (user?.uid)
+			dispatch({ type: 'SIGN_IN', token: user.uid });
+	};
 
-    useEffect(() => {
-        GoogleSignin.configure({
-            webClientId: "265977501020-dq10qkihc4h0nn37cmnm38l3m4l6adkr.apps.googleusercontent.com",
-        });
+	useEffect(() => {
+		GoogleSignin.configure({
+			webClientId: "265977501020-dq10qkihc4h0nn37cmnm38l3m4l6adkr.apps.googleusercontent.com",
+		});
+	}, []);
 
-    }, []);
+	useEffect(() => {
+		const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+		return subscriber; // unsubscribe on unmount
+	}, []);
 
-    useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-        return subscriber; // unsubscribe on unmount
-    }, []);
+	const authContext = useMemo(
+		() => ({
+			signIn: async (data) => {
+				googleSignIn()
+			},
+			signOut: () => {
+				googleSignOut().then(
+					dispatch({ type: 'SIGN_OUT'})
+				);
+			},
+			userToken: state.userToken
+		}),
+		[state.userToken]
+	);
 
+	console.log(state, auth().currentUser.uid);
 
-    const authContext = useMemo(
-        () => ({
-            signIn: async (data) => {
-                googleSignIn()
-            },
-            signOut: () => {
-                googleSignOut().then(
-                    dispatch({ type: 'SIGN_OUT'})
-                );
-            },
-            userToken: state.userToken
-        }),
-        [state.userToken]
-    );
+	if (state.isLoading) {
+		return <ActivityIndicator/>
+	}
 
-    if (state.isLoading) {
-        return <ActivityIndicator/>
-      }
-
-    return (
-        <AuthContext.Provider value={authContext}>
-            <NavigationContainer>
-                <Stack.Navigator>
-                    {state.userToken == null ? (
-                        <Stack.Screen name="LOGIN" component={LoginScreen} />
-                    ) : (
-                        <Stack.Screen name="MAIN" options={{headerShown: false}} component={MainScreen}/>
-                    )}
-                </Stack.Navigator>
-            </NavigationContainer>
-        </AuthContext.Provider>
-    );
+	return (
+		<AuthContext.Provider value={authContext}>
+			<NavigationContainer>
+				<Stack.Navigator>
+					{state.userToken == null ? (
+						<Stack.Screen name="LOGIN" component={LoginScreen} />
+					) : (
+						<>
+							<Stack.Screen name="MAIN" options={{headerShown: false}} component={MainScreen}/>
+							<Stack.Screen name="DETAIL" 
+								component={DetailScreen}
+								options={{ 
+									headerStyle: {height: Platform.OS === "android" ? 60:105, }, 
+									headerLeft: ()=>
+										<Icon 
+											name={'arrow-left'} 
+											color={"black"} 
+											onPress={ () => { navigation.goBack() }} 
+											style={{ marginLeft: 24 }} size={24}
+										/>,
+									headerTitle: "", 
+								}}
+							/>
+						</>
+						
+					)}
+				</Stack.Navigator>
+			</NavigationContainer>
+		</AuthContext.Provider>
+	);
 };
-
-
-
-
-
-
-// const App_2 = () => {
-//     const [initializing, setInitializing] = useState(true);
-//     const [user, setUser] = useState();
-  
-//     // Handle user state changes
-//     function onAuthStateChanged(user) {
-//       setUser(user);
-//       if (initializing) setInitializing(false);
-//     }
-
-//     useEffect(() => {
-//         GoogleSignin.configure({
-//             webClientId: "265977501020-dq10qkihc4h0nn37cmnm38l3m4l6adkr.apps.googleusercontent.com",
-//         });
-//     }, []);
-  
-//     useEffect(() => {
-//       const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-//       return subscriber; // unsubscribe on unmount
-//     }, []);
-  
-//     if (initializing) return <ActivityIndicator />; // Logo should be displayed here
-
-//     if (!user) {
-//         return (
-//             <View style={{alignItems: "center", top: 250}}>
-//                 <GoogleSigninButton onPress={() => onGoogleButtonPress()} />
-//             </View>
-//         );
-//     }
-    
-//     console.log(user)
-//     return (
-//         <Button 
-//             onPress={googleSignOut}
-//             title={"logout"}
-//         />
-//     )
-//     return (
-//         <NavigationContainer>
-//             <StackNavigation />
-//         </NavigationContainer>
-//     )
-// }
 
 export default App;
 
--- ProjectName/src/screens/MainScreen.js
+++ ProjectName/src/screens/MainScreen.js
@@ -1,10 +1,10 @@
 import React, {useState, useEffect, useContext} from 'react';
-import {Button, Platform, SafeAreaView,Dimensions, ScrollView,Text,View,Image,TouchableOpacity, Modal, KeyboardAvoidingView, TextInput} from 'react-native';
+import {Button, Platform, SafeAreaView,Dimensions, ScrollView,Text,View,Image,TouchableOpacity, Modal, KeyboardAvoidingView, TextInput, ActivityIndicator} from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
 import Icon from 'react-native-vector-icons/Feather';
 import { styles } from './Style';
 import { AuthContext } from '../App';
-
+import database from '@react-native-firebase/database';
 
 
 const UnsignedHeader = ({onPress}) => (
@@ -37,9 +37,22 @@ const AppButton = ({ navigation, info }) => (
 );
 
 const Footer = ({onPress}) => (
-  <View style ={{borderColor: "#EDEDEE", borderTopWidth: 1, paddingHorizontal: 32, paddingTop: 16,height: 102, width: "100%", zIndex: 100}}>
+  <View style ={{
+    borderColor: "#EDEDEE", 
+    borderTopWidth: 1, 
+    paddingHorizontal: 32, 
+    paddingTop: 16,
+    height: 102, 
+    width: "100%", 
+    zIndex: 100
+  }}>
     <TouchableOpacity onPress={onPress}>
-      <View style={{backgroundColor: "#4769EE", paddingVertical: 16, alignItems: "center", borderRadius: 16}}>
+      <View style={{
+        backgroundColor: "#4769EE", 
+        paddingVertical: 16, 
+        alignItems: "center", 
+        borderRadius: 16
+      }}>
         <Text style={{color: "white", fontSize: 16, fontWeight: "700"}}>관람 인증하기</Text>
       </View>
     </TouchableOpacity>
@@ -47,13 +60,27 @@ const Footer = ({onPress}) => (
 );
 
 const Cert_Modal = ({onPress, OpenCert, onPress_cert})=>(
-  <Modal animationType='fade' visible={OpenCert} transparent={true} presentationStyle='overFullScreen' style={{zIndex: 1,}}>
-    <KeyboardAvoidingView behavior="position" style={{ height: Platform.OS === 'android' ? Dimensions.get('window').height: '100%',}}>
+  <Modal 
+    animationType='fade' 
+    visible={OpenCert} 
+    transparent={true} 
+    presentationStyle='overFullScreen' 
+    style={{zIndex: 1,}}
+  >
+    <KeyboardAvoidingView 
+      behavior="position" 
+      style={{ height: Platform.OS === 'android' ? Dimensions.get('window').height: '100%',}}
+    >
       <TouchableOpacity onPress={onPress}>
         <View style={styles.modal}>
           <View style ={styles.modalwhitepart}>
             <Text style={styles.textinmodal}>티켓 코드 입력</Text>
-            <TextInput eyboardType='numeric' placeholder="티켓 앞장 하단의 코드를 입력하세요" placeholderTextColor="#B1AEAE" style={styles.textinputbox}></TextInput>
+            <TextInput 
+              eyboardType='numeric' 
+              placeholder="티켓 앞장 하단의 코드를 입력하세요" 
+              placeholderTextColor="#B1AEAE" 
+              style={styles.textinputbox}
+            />
             <Footer onPress={onPress_cert}></Footer>
           </View>
         </View>
@@ -64,25 +91,60 @@ const Cert_Modal = ({onPress, OpenCert, onPress_cert})=>(
 
 const MainScreen= ({navigation}) => {
 
-  const { signOut } = useContext(AuthContext);
+  const { signOut, userToken } = useContext(AuthContext);
 
-  const [isLoading, setIsLoading] = useState(true);
-  const [currentExhibit, setCurrentExhibit] = useState('성수/AWA');
-  
+  const [isLoadingStores, setIsLoadingStores] = useState(true);
+  const [isLoadingCert, setIsLoadingCert] = useState(true);
+  const [isLoadingExhibitNames, setIsLoadingExhibitNames] = useState(true);
+  const [exhibitNames, setExhibitNames] = useState([]);
+  const [currentExhibit, setCurrentExhibit] = useState("");
+  const [storeList, setStoreList] = useState([]);
+  const [certificateList, setCertificateList] = useState({});
+
   useEffect(() => {
-    setIsLoading(true)
-    setTimeout(()=>setIsLoading(false), 1000);
+    database()
+      .ref('/exhibitionNames/')
+      .once('value')
+      .then(snapshot => {
+        if (snapshot.val()) {
+          setExhibitNames(snapshot.val());
+          setCurrentExhibit(snapshot.val()[0].replace('_', '/'));
+        }
+        setIsLoadingExhibitNames(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!currentExhibit) return;
+    setIsLoadingStores(true);
+    database()
+      .ref(`/exhibitions/${currentExhibit.replace('/', '_')}`)
+      .once('value')
+      .then(snapshot => {
+        if (snapshot.val())
+          setStoreList(snapshot.val());
+        setIsLoadingStores(false);
+      })
   }, [currentExhibit]);
-  
-  const amount = 7 // amounts of shops
 
-  var row_step
-  const id = []
+  useEffect(() => {
+    setIsLoadingCert(true);
+    database()
+      .ref(`/users/${userToken}/certificate/`)
+      .on('value', snapshot => {
+        if (snapshot.val())
+          setCertificateList(snapshot.val());
+        setIsLoadingCert(false);
+      })
+  }, []);
 
-  for(row_step=1; row_step<amount/2; row_step++){
-    id.push([1,1]);
+  const id = [];
+  const numStore = storeList.length;
+
+  for(var row_step=1; row_step<=numStore/2; row_step++){
+    id.push([row_step*2-1, row_step*2]);
   }
-  if(amount%2==1){id.push([1,0]);}
+  if(numStore%2==1) {id.push([numStore, 0]);}
 
   const info = {
     storeName: '쏘리에스프레소바',
@@ -94,10 +156,12 @@ const MainScreen= ({navigation}) => {
 
   const buttonList = id.map((button)=> (
     <View style ={styles.RowContainer}>
-      {button[0] ?  <AppButton navigation={navigation} info={info}/> : <View/>}
-      {button[1] ?  <AppButton navigation={navigation} info={info}/> : <View/>}
+      {button[0] ?  
+        <AppButton navigation={navigation} info={storeList[button[0]-1]}/> : <View/>}
+      {button[1] ?  
+        <AppButton navigation={navigation} info={storeList[button[1]-1]}/> : <View/>}
     </View>)
-  )
+  );
   const seesaw = ["성수/AWA", "서촌/Red Room", "명동/Poethic AI"]
   const [Certificate, setCertificate] = useState(false); //해당 계정이 관람 인증이 되었는지
   const [OpenCert, setOpenCert] = useState(false); // 티켓 인증 모달의 상태
@@ -106,19 +170,36 @@ const MainScreen= ({navigation}) => {
     <SafeAreaView style={{flex: 1, paddingTop: Platform.OS === 'android' ? 8 : 0,}}>
       <View style ={styles.Header}>
         <View style ={styles.DefaultHeader}>
-          <SelectDropdown data={seesaw} onSelect={(selectedItem, index) => {console.log(selectedItem, index)}}
-          buttonTextAfterSelection={(selectedItem) => {return selectedItem}} rowTextForSelection={(item) => {return item}}
-          renderDropdownIcon ={() => {
-            return (<Icon name="chevron-down" color={"black"} size={18} style={{marginRight: 32}}/>);}}
-          dropdownIconPosition="right"/>
+          {
+            isLoadingExhibitNames ? <ActivityIndicator/> : (
+              <SelectDropdown 
+                data={exhibitNames} 
+                onSelect={(selectedItem) => {setCurrentExhibit(selectedItem)}}
+                buttonTextAfterSelection={(selectedItem) => {return selectedItem}} 
+                rowTextForSelection={(item) => {return item}}
+                renderDropdownIcon ={() => {
+                  return (
+                    <Icon name="chevron-down" color={"black"} size={18} style={{marginRight: 32}}/>
+                  );
+                }}
+                dropdownIconPosition="right"
+                defaultButtonText={currentExhibit}
+              />
+            )
+          }
         </View>
-        {/* {Certificate ? <View/>:<UnsignedHeader onPress={()=> {setOpenCert(true)}}/>} */}
-        {Certificate ? <View/>:<UnsignedHeader onPress={signOut}/>}
+        {isLoadingCert ? <ActivityIndicator/>
+          : (certificateList?.currentExhibit ? <View/>:<UnsignedHeader onPress={()=>setOpenCert(true)}/>)}
       </View>
-      <ScrollView style={{zIndex: 0}}>
-          <View style={{paddingBottom: 32}}>{buttonList}</View>
-      </ScrollView>
-      <Cert_Modal OpenCert={OpenCert} onPress={()=> {setOpenCert(false)}} onPress_cert={()=> {setCertificate(true); setOpenCert(false);}}/>
+      {isLoadingStores ? <ActivityIndicator/>
+        : (<ScrollView style={{zIndex: 0}}>
+            <View style={{paddingBottom: 32}}>{buttonList}</View>
+          </ScrollView>)}
+      
+      <Cert_Modal 
+        OpenCert={OpenCert} 
+        onPress={()=> {setOpenCert(false)}} 
+        onPress_cert={()=> {setCertificate(true); setOpenCert(false);}}/>
     </SafeAreaView>
   );
 };
