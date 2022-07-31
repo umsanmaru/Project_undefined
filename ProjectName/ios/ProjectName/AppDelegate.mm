a9ef21a6c046255393c88a44ec533be035ddae64
kyllene@kaist.ac.kr <77186601+dooho0915@users.noreply.github.com>
2022-07-31 23:10:15 +0900

    0731 dooho
    fix working in ios, app icon added

--- ProjectName/ios/ProjectName/AppDelegate.mm
+++ ProjectName/ios/ProjectName/AppDelegate.mm
@@ -6,7 +6,7 @@
 #import <React/RCTRootView.h>
 
 #import <React/RCTAppSetupUtils.h>
-
+#import <RNKakaoLogins.h>
 #if RCT_NEW_ARCH_ENABLED
 #import <React/CoreModulesPlugins.h>
 #import <React/RCTCxxBridgeDelegate.h>
@@ -27,7 +27,15 @@
 #endif
 
 @implementation AppDelegate
+- (BOOL)application:(UIApplication *)app
+     openURL:(NSURL *)url
+     options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+ if([RNKakaoLogins isKakaoTalkLoginUrl:url]) {
+    return [RNKakaoLogins handleOpenUrl: url];
+ }
 
+ return NO;
+}
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     [FIRApp configure];
